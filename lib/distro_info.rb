620e975982080fdcfefcf79d439dd6eace77cc22
Henzi <mugunga5@gmail.com>
2020-05-10 18:30:50 +0200

    Add a method to select distro list

--- lib/distro_info.rb
+++ lib/distro_info.rb
@@ -1,26 +1,35 @@
 require "nokogiri"
 require "httparty"
 require "httpclient"
-require "pry"
 require "open-uri"
-require "watir"
 
 class Distros
   attr_accessor :input
-
-  def initialize(input)
+  attr_reader :all
+  def initialize
     @input = input
+    @all = Array.new
   end
+
+
 
   def scraper
     url = "https://distrowatch.com/index.php?dataspan=#{@input}"
     document = open(url)
     content = document.read
     parsed = Nokogiri::HTML(content)
-    list = parsed.css('table.Logo table.News td.phr2 a')
-    puts list
+    list = parsed.css("table.Logo table.News td.phr2 a")
+    (0...list.length).each do |distro|
+      @all << list[distro].text
+    end
+    @all
   end
+
 end
 
-distros = Distros.new(2019)
+ distros = Distros.new
+puts "Enter year"
+year = gets.to_i
+distros.input = year
 distros.scraper
+puts distros.all
