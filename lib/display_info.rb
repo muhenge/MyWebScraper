f2e33e1dd274508693929fae23c2b21b5d57d54a
Henzi <mugunga5@gmail.com>
2020-05-12 05:54:17 +0200

    Change mathod scraper

--- lib/display_info.rb
+++ lib/display_info.rb
@@ -1,2 +1,24 @@
-require "display_info.rb"
+require "nokogiri"
+require "httparty"
+require "httpclient"
+require "open-uri"
 
+class Distros_info
+  attr_reader :name
+  def initialize(name)
+    @name = name
+  end
+  
+  def distro_info
+    url = "https://distrowatch.com/table.php?distribution=#{@name}"
+    puts url
+    document = open(url)
+    content = document.read
+    parsed = Nokogiri::HTML(content)
+    info = parsed.css("td.TablesTitle ul li a")
+    puts info
+  end
+end
+
+distro = Distros_info.new('manjoro')
+distro.distro_info
