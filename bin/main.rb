98fa7328572d0420928fe7a19bd169d8a262800a
Henzi <mugunga5@gmail.com>
2020-05-12 22:23:32 +0200

    Add choose_one method and start method in the main file

--- bin/main.rb
+++ bin/main.rb
@@ -1,60 +1,114 @@
 require "../lib/distro_info.rb"
-distros = Distros.new
+require "../lib/display_info.rb"
+require "date"
 
 def show_all
-  distros = Distros.new
-  puts ""
-  puts "Hello, Welcome!"
-  puts "I am going to show you the all linux distro list based on the usage"
-  puts "-------------------------------------------------------------------"
-  puts "Please Enter the year you want, to display the list"
-  year = gets.chomp
-  distros.input = year
-  distros.scraper
-  puts distros.all
+  distro = Distros.new
+  puts "-----------------------------------------------------"
+  puts "Enter any year you want between 2011 till now, you will see a ranking list : or 0 to go back"
+  year = gets.to_i
+  while year
+    case year
+    when 0
+      start
+    when 2011..(Date.today.year).to_i
+      distro.input = year
+      puts distro.show
+      puts ""
+      start
+    else
+      puts "Year beyond scope, year must be between 2011 to today's year"
+      year = gets.to_i
+      next
+    end
+  end
 end
 
-def choose_one
-   distros = Distros.new
-  # puts "enter year"
-  # year = gets.to_i
-  # distros.input = year
-  # puts "distro name"
-  # name = gets.chomp
-  # puts (distros.all).include?(name)
-  puts distros.all
-end
-choose_one
-
-# puts ""
-# puts "This program can help you to rate Linux distibution, and help you to make decisions.\nit is comparing a disto that you want based on the past years"
-# puts "---------------------------------------------------------------------------------------------------------------------\n\n"
-# puts "Please select an option: "
-# puts ""
-# puts "1. Show all Linux distros. "
-# puts "2. show my linux of choice."
-# puts "3. compare my linux distro."
-
-
-
-
-# puts "Enter year"
-# year = gets.chomp
-# distros.input = year
-# distros.scraper
-# puts distros.all
-
-# year = gets.to_i
-# distros.input = year
-# distros.view
-
-# arr = [4, 5, 6, 7, 2, 3, 10]
-# puts "Please Enter a Linux distro see informarion and recommendation"
-# user_input = gets.to_i
-# while arr.include?(user_input) == false
-#   puts "not valid"
-#   user_input = gets.to_i
-#   show if arr.include?(user_input)
+#ssss
+#show_all
+#def compare
+#   distro = Distros.new
+#   today_distro = Todays.new
+#   puts "Please Enter the name of the distro to compare"
+#   name = gets.chomp.capitalize
+#   last_year = (Date.today.year)-1
+#   distro.input = last_year
+#   last_year_index = (distro.show).index(name)
+#   today_index = (today_distro.today_show).index
+#   differance = today_index - last_year_index
+#   if today_show <=> last_year_index = 1
+#     puts "It seems #{name} Distro is gaining #{differance} places today"
+#   end
 # end
 
-# distro_chooser
+# compare
+def choose_one
+  distro = Distros.new
+  all_distro = distro.show
+  
+  puts "----------------------------------------------"
+  puts ""
+  puts "Enter the year"
+  year = gets.to_i
+  while year
+    case year
+    when 0
+      start
+    when 2011..(Date.today.year).to_i
+      puts "Enter a distro name"
+      name = gets.chomp.capitalize
+      index = (all_distro).index(name)
+      while name
+        if all_distro.include?(name) == true
+          puts ""
+          puts "--------------------------------------"
+          puts "Found!"
+          puts "'#{name}' is at position '#{index}'"
+          puts "--------------------------------------"
+          puts ""
+          break
+          start
+        else
+          puts "'#{name}' not found, try again"
+          puts ""
+          choose_one
+        end
+      end
+    else
+      puts "Year beyond scope, year must be between 2011 to today's year"
+      year = gets.to_i
+      next
+    end
+  end
+end
+
+def start
+puts ""
+  puts "Hello, this program is helping you to choose a linux distribition based on the popurality and users rating"
+  puts "---------------------------------------"
+  puts ""
+
+  puts "Enter an option to start"
+  puts ""
+  puts ""
+  puts "1. Show all Linux distros. "
+  puts "2. show my linux of choice."
+  puts ""
+  print ""
+  choice = gets.to_i
+
+  while choice
+    case choice
+    when 1
+      show_all
+    when 2
+    choose_one
+    else
+      puts "Wrong option! Try again 1 or 2"
+      choice = gets.to_i
+    next
+    end
+  end
+end
+
+start
