c86bfb4546c81cd3182d1bf2267cbc2790c2ba2f
Henzi <mugunga5@gmail.com>
2020-05-12 06:28:42 +0200

    add show method in Distro class and private scraper method

--- lib/distro_info.rb
+++ lib/distro_info.rb
@@ -5,31 +5,25 @@ require "open-uri"
 
 class Distros
   attr_accessor :input
-  attr_reader :all
-  def initialize
-    @input = input
-    @all = Array.new
-  end
 
-
+  private
 
   def scraper
-    url = "https://distrowatch.com/index.php?dataspan=#{@input}"
+    url = "https://distrowatch.com/index.php?dataspan=#{input}"
     document = open(url)
     content = document.read
     parsed = Nokogiri::HTML(content)
-    list = parsed.css("table.Logo table.News td.phr2 a")
-    (0...list.length).each do |distro|
-      @all << list[distro].text
-    end
-    @all
+    @list = parsed.css("table.Logo table.News td.phr2 a")
   end
 
+  public
+  
+  def show
+    scraper
+    all = Array.new
+    (0...@list.length).each do |distro|
+      all << @list[distro].text
+    end
+    all
+  end
 end
-
- distros = Distros.new
-puts "Enter year"
-year = gets.to_i
-distros.input = year
-distros.scraper
-puts distros.all
