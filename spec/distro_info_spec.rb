21188a2c7a811be7a36858eee77a8089fd3390cd
Henzi <mugunga5@gmail.com>
2020-05-13 14:16:40 +0200

    Correct lint errors

--- spec/distro_info_spec.rb
+++ spec/distro_info_spec.rb
@@ -3,13 +3,13 @@
 require '../lib/distro_info'
 
 describe Distros do
-    describe '#show' do
-      context "a method returns an array" do
-        let(:distro) { Distros.new } 
-        it 'a public method returns an array' do
-        expect(distro.show).
-        to be_an_instance_of(Array)
+  describe '#show' do
+    context 'a method returns an array' do
+      let(:distro) { Distros.new }
+      it 'a public method returns an array' do
+        expect(distro.show)
+          .to be_an_instance_of(Array)
       end
     end
-end
+  end
 end
